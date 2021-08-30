e6d4bd85faa502bcfa6961ce8221976fe9615e53
Marius <marius.horeau@activus-group.fr>
2021-08-30 15:47:58 +0200

    create component navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavigationMenuComponent } from './components/side-navigation-menu/side-navigation-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavigationMenuComponent
   ],
   imports: [
     BrowserModule,
