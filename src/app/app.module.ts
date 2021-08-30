7023030b1ddc076006d773466ce392d4a8cf6c2e
O_Nocaudie <olivier.nocaudie@activus-group.fr>
2021-08-30 12:08:23 +0200

    init empty components for the view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientViewComponent } from './client-view/client-view.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
+import { RestaurantMapComponent } from './restaurant-map/restaurant-map.component';
+import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.component';
+import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientViewComponent,
+    RestaurantListComponent,
+    RestaurantMapComponent,
+    RestaurantMenuComponent,
+    RestaurantCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/restaurant-map/restaurant-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-map',
+  templateUrl: './restaurant-map.component.html',
+  styleUrls: ['./restaurant-map.component.css']
+})
+export class RestaurantMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
