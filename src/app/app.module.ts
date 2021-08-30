cad7ce6276402cd9868c8fcfb5ee45389337d8cd
O_Nocaudie <olivier.nocaudie@activus-group.fr>
2021-08-30 13:20:35 +0200

    file tree modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientViewComponent } from './client-view/client-view.component';
-import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
-import { RestaurantMapComponent } from './restaurant-map/restaurant-map.component';
-import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.component';
-import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+import { ClientViewComponent } from './view/client-view/client-view.component';
+import { RestaurantListComponent } from './components/restaurant-list/restaurant-list.component';
+import { RestaurantMapComponent } from './components/restaurant-map/restaurant-map.component';
+import { RestaurantMenuComponent } from './components/restaurant-menu/restaurant-menu.component';
+import { RestaurantCardComponent } from './components/restaurant-card/restaurant-card.component';
 
 @NgModule({
   declarations: [
--- src/app/views/client-view/client-view.component.ts
+++ src/app/views/client-view/client-view.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientViewComponent implements OnInit {
 
+  public restaurants: any = [];
+
   constructor() { }
 
   ngOnInit() {
