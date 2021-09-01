aafabd82a8b53663d8c84d02328a7ac139a902fa
O_Nocaudie <olivier.nocaudie@activus-group.fr>
2021-09-01 14:01:04 +0200

    file tree refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,14 @@ import { RestaurantListComponent } from './views/client-view/restaurant-list/res
 import { RestaurantMapComponent } from './views/client-view/restaurant-map/restaurant-map.component';
 import { RestaurantMenuComponent } from './views/client-view/restaurant-menu/restaurant-menu.component';
 import { RestaurantCardComponent } from './views/client-view/restaurant-card/restaurant-card.component';
-import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { MenuItemComponent } from './views/client-view/menu-item/menu-item.component';
 import { ToTableReservationComponent } from './views/client-view/to-table-reservation/to-table-reservation.component';
 import { ReservationFormComponent } from './views/client-view/reservation-form/reservation-form.component';
 import { AdminViewComponent } from './views/admin-view/admin-view.component';
 import { ManagerViewComponent } from './views/manager-view/manager-view.component';
 import { CookViewComponent } from './views/cook-view/cook-view.component';
 import { WaiterViewComponent } from './views/waiter-view/waiter-view.component';
-import { ClientSelectorComponent } from './components/client-selector/client-selector.component';
+import { ClientSelectorComponent } from './views/client-view/client-selector/client-selector.component';
 import { SubMenuComponent } from './components/sub-menu/sub-menu.component';
 import { FriendsListComponent } from './views/client-view/friends-list/friends-list.component';
 import { UserFinderComponent } from './user-finder/user-finder.component';
