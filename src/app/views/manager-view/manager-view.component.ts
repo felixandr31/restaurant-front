901798dbe99014e92c10ddaa955c4cbbbf73a9ab
Marius <marius.horeau@activus-group.fr>
2021-09-01 13:41:22 +0200

    manager-view in progress commit before merge

--- src/app/views/manager-view/manager-view.component.ts
+++ src/app/views/manager-view/manager-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-manager-view',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager-view.component.css']
 })
 export class ManagerViewComponent implements OnInit {
+
+  @Input() showSubView: any;
 
   constructor() { }
 
