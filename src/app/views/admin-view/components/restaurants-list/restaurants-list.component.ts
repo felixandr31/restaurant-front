177e7eabf5867ef244b4417f3e4de9d340ed389d
Marius <marius.horeau@activus-group.fr>
2021-09-09 22:56:00 +0200

    Feat: Introduced somme basics aAdmin view features fix: managerview employee selection (template data was not identical to DB data)

---
+++ src/app/views/admin-view/components/restaurants-list/restaurants-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-list',
+  templateUrl: './restaurants-list.component.html',
+  styleUrls: ['./restaurants-list.component.css']
+})
+export class RestaurantsListComponent implements OnInit {
+
+  @Input() restaurants: any;
+  @Output() onRestaurantSelection = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  restaurantSelected(event) {
+    this.onRestaurantSelection.emit(event.target.value);
+  }
+
+}
