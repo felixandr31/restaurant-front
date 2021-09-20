4b3b8ef402725c4c67ddd79af55b4b31875388ef
Hermann08 <78152998+Hermann08@users.noreply.github.com>
2021-09-20 15:08:43 +0200

    fix  waiterService

---
+++ src/app/views/waiter-view/select-booking/select-booking.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DateAdapter } from '@angular/material';
+
+@Component({
+  selector: 'app-select-booking',
+  templateUrl: './select-booking.component.html',
+  styleUrls: ['./select-booking.component.css']
+})
+export class SelectBookingComponent implements OnInit {
+
+  @Output() onDateSelection = new EventEmitter();
+
+
+  constructor(private formBuilder: FormBuilder, private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('fr-FR')
+  }
+
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      day: [new Date(), Validators.required],
+    })
+
+
+  }
+
+  dateSelected() {
+    const date: Date = this.form.value.day
+
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString().length == 1 ? '0' + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString()
+    const day = date.getDate() < 10 ? '0' + date.getDate().toString() : date.getDate().toString()
+    const dayOfVenue = `${year}-${month}-${day}`
+
+    let dayTime = {
+      day: dayOfVenue,
+
+    }
+    console.log('date selector event', dayTime)
+    this.onDateSelection.emit(dayTime)
+  }
+
+
+}
