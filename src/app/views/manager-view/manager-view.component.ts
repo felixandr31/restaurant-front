21126fc4e5309623b629c668681bf0281b985be5
Safia <safia.slimani@activus-group.fr>
2021-09-20 13:34:50 +0200

    ajout service ingredient

--- src/app/views/manager-view/manager-view.component.ts
+++ src/app/views/manager-view/manager-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RestaurantService } from 'src/app/services/data/restaurant.service';
+import { IngredientService } from 'src/app/services/data/ingredient.service';
 
 @Component({
   selector: 'app-manager-view',
@@ -14,34 +15,45 @@ export class ManagerViewComponent implements OnInit {
 
   public managerRestaurant: any;
   public stocks: any;
+  public ingredients : any;
 
-  constructor(private restaurantService: RestaurantService) {
+  constructor(private restaurantService: RestaurantService, private ingredientService: IngredientService) {
   }
 
   ngOnInit() {
     this.showSubView = 'homePage';
-    const managerRestaurantId = ["6131c91756aac85ca96e1197"]
-
+    //const managerRestaurantId = ["6131c91756aac85ca96e1197"]
     
-
-    this.restaurantService.getStocks().subscribe(
+    this.reloadRestaurant();
+    // console.log('managerRestaurant: ', this.managerRestaurant)
+    // this.stocks = Object.assign({}, this.managerRestaurant.stocks)
+    
+    //  this.restaurantService.getStocks().subscribe(
+    //    data => {
+    //      this.stocks = data.body;
+    //      console.log(this.stocks)
+    //    },
+    //    err => {
+    //      console.log('erreur', err)
+    //    }
+    //  )
+    this.ingredientService.getIngredient().subscribe(
       data => {
-        this.stocks = data.body;
-        console.log(this.stocks)
+        this.ingredients = data.body;
+        console.log(this.ingredients)
       },
       err => {
         console.log('erreur', err)
       }
     )
 
-
-    this.reloadRestaurant();
   }
 
   reloadRestaurant() {
     return this.restaurantService.getRestaurantById(this.user.restaurantId).subscribe(
       data => {
-        this.managerRestaurant = Object.assign({}, data.body)
+        console.log(data.body)
+        this.managerRestaurant = {...data.body}
         console.log('reload restaurant')
       })
   }
