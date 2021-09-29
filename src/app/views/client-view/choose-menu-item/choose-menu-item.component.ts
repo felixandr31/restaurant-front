fdf24238b6086a6a355f77a140af816cd0862033
Olivier Nocaudie <olivier.nocaudie@activus-group.fr>
2021-09-29 17:28:32 +0200

    made logic to check stocks while choosing items in the menu probably need to refactor it to work in the parent component where everything is centralized

--- src/app/views/client-view/choose-menu-item/choose-menu-item.component.ts
+++ src/app/views/client-view/choose-menu-item/choose-menu-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-choose-menu-item',
@@ -9,28 +10,35 @@ export class ChooseMenuItemComponent implements OnInit, OnChanges {
 
   @Input() item: any;
   @Input() bill: any;
+  @Input() cachedStocks: any;
   @Output() onItemAdd = new EventEmitter();
   @Output() onItemRemove = new EventEmitter();
 
   public line: any = {name: '', quantity: 0, sellingPrice: 0};
   public quantity = 0
+  public availableItem: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-    console.log('item', this.item)
+
   }
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
     if (this.bill.length > 0 && this.bill.find(e => e.item.name == this.item.name)) {
       this.updateLine();
       this.quantity = this.line.quantity
+    }
+    if (this.cachedStocks.length) {
+      this.availabilityCheck(this.cachedStocks)
+      console.log('available ?', this.item.name, this.availableItem)
     }
   }
 
 
 addItem(event) {
   this.onItemAdd.emit(event.target.value);
+  console.log(event.target.value)
 }
 
 removeItem(event) {
@@ -38,8 +46,21 @@ removeItem(event) {
 }
 
   updateLine() {
-
     const lineUpdated = this.bill.find(e => e.item.name === this.item.name)
     return this.line = lineUpdated
+  }
+
+  availabilityCheck(stock) {
+    this.availableItem = true;
+    this.item.ingredientsRecipe.forEach(ingR => {
+      stock.forEach(lul => {
+        if (lul.ingredient.id === ingR.ingredient.id) {
+          if(lul.quantity < ingR.quantity) {
+            console.log('missing ingredient', lul.ingredient, lul.quantity)
+            return this.availableItem = false
+          }
+        }
+      })
+    })
   }
 }
--- src/app/views/client-view/choose-menu/choose-menu.component.ts
+++ src/app/views/client-view/choose-menu/choose-menu.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { share } from 'rxjs/operators'
 
 @Component({
   selector: 'app-choose-menu',
   templateUrl: './choose-menu.component.html',
   styleUrls: ['./choose-menu.component.css']
 })
-export class ChooseMenuComponent implements OnInit {
+export class ChooseMenuComponent implements OnInit, OnChanges {
 
   @Input() restaurant: any;
   @Input() bill: any;
@@ -13,20 +15,44 @@ export class ChooseMenuComponent implements OnInit {
   @Output() onItemRemoved = new EventEmitter();
 
   public order = [];
-
-  public stocks = []
+  public cachedStocks;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes', changes)
+    if (this.restaurant.id) {
+      this.cachedStocks = this.restaurant.stocks;
+      console.log('stocks', this.cachedStocks)
+  }
+}
+
   itemAdded(event) {
     this.onItemAdded.emit(event);
+    const currentRecipe = this.restaurant.recipes.find(recipe => recipe.name === event)
+    currentRecipe.ingredientsRecipe.forEach(ingR => {
+      this.cachedStocks.forEach(stock => {
+        if (stock.ingredient.id === ingR.ingredient.id) {
+          stock.quantity -= ingR.quantity
+        }
+      })
+    })
+    this.cachedStocks = this.cachedStocks.slice(0)
   }
 
   itemRemoved(event) {
     this.onItemRemoved.emit(event)
+    const currentRecipe = this.restaurant.recipes.find(recipe => recipe.name === event)
+    currentRecipe.ingredientsRecipe.forEach(ingR => {
+      this.cachedStocks.forEach(stock => {
+        if (stock.ingredient.id === ingR.ingredient.id) {
+          stock.quantity += ingR.quantity
+        }
+      })
+    })
+    this.cachedStocks = this.cachedStocks.slice(0)
   }
-
 }
